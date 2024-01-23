10a0f5937ddfc195504bd27ae83dda9037665f97
Miray Buyukkaray <miraybuyukkaray@gmail.com>
2024-01-23 13:57:23 +0100

    authorization updates

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,15 +5,18 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Task;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-      Task::factory(10)->create();
-    }
+  /**
+   * Seed the application's database.
+   */
+  public function run(): void
+  {
+    User::factory(5)->has(
+      Task::factory(10)
+    )->create();
+  }
 }
